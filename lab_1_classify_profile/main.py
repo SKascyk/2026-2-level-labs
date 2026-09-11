74ad0ffc25d840cd9ac3e184c502b82c075e4fd8
Stanislau Kascyk <senitinta@yandex.ru>
2026-09-11 09:45:19 +0300

    The function lab_1_classify_profile.main.remove_stop_words() has been defined

--- lab_1_classify_profile/main.py
+++ lab_1_classify_profile/main.py
@@ -44,7 +44,11 @@ def remove_stop_words(tokens: Sequence[str], stop_words: Sequence[str]) -> Seque
     # Returns:
     #     Sequence[str] | None: Sequence of tokens without stop words.
     #     Returns None in case of incorrect input types.
-
+    if (not isinstance(tokens, Sequence)) or (not isinstance(stop_words, Sequence)):
+        return None
+    
+    cleaned_tokens = [word for word in tokens if word not in stop_words]
+    return cleaned_tokens
 
 
 def calculate_frequencies(tokens: Sequence[str]) -> dict[str, float] | None:
