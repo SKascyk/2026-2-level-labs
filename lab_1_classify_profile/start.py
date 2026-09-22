cbc4af8a653dd782d60e618ad772028f3e1ff16a
Stanislau Kascyk <senitinta@yandex.ru>
2026-09-22 12:14:26 +0300

    Necessary functions imported from the module main.py

--- lab_1_classify_profile/start.py
+++ lab_1_classify_profile/start.py
@@ -3,7 +3,7 @@ Language detection starter.
 """
 
 # pylint: disable=unused-variable, duplicate-code
-import lab_1_classify_profile.main
+from lab_1_classify_profile.main import create_language_profile, detect_language_by_top_n
 
 
 def main() -> None:
@@ -18,10 +18,10 @@ def main() -> None:
         stopwords = file.read().split("\n")
     with open("lab_1_classify_profile/assets/texts/en.txt", "r", encoding="utf-8") as file:
         en_text = file.read()
-    de_profile = lab_1_classify_profile.main.create_language_profile('de', de_text, stopwords)
-    en_profile = lab_1_classify_profile.main.create_language_profile('en', en_text, stopwords)
-    unk_profile = lab_1_classify_profile.main.create_language_profile('unknown', unknown_text, stopwords)
-    result = lab_1_classify_profile.main.detect_language_by_top_n(unk_profile, de_profile, en_profile, 15)
+    de_profile = create_language_profile('de', de_text, stopwords)
+    en_profile = create_language_profile('en', en_text, stopwords)
+    unk_profile = create_language_profile('unknown', unknown_text, stopwords)
+    result = detect_language_by_top_n(unk_profile, de_profile, en_profile, 15)
     assert result, "Detection result is None"
     print(result)
 
