d05a75d87eea3db4ec8214ddc41595b2c9cc0dad
Stanislau Kascyk <senitinta@yandex.ru>
2026-09-23 20:09:43 +0300

    The type checking has been added in lab_1_classify_profile\start.py

--- lab_1_classify_profile/start.py
+++ lab_1_classify_profile/start.py
@@ -21,7 +21,15 @@ def main() -> None:
     de_profile = create_language_profile('de', de_text, stopwords)
     en_profile = create_language_profile('en', en_text, stopwords)
     unk_profile = create_language_profile('unknown', unknown_text, stopwords)
-    result = detect_language_by_mse(unk_profile, de_profile, en_profile)
+    result = (
+        detect_language_by_mse(unk_profile, de_profile, en_profile)
+        if not(
+            unk_profile is None
+            or de_profile is None
+            or en_profile is None
+        )
+        else None
+    )
     assert result, "Detection result is None"
     print(result)
 
